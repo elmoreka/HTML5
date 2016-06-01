81a2008cf647a50b507b728b2015a6a2de532b68
Karl Elmore <Karl Elmore>
2016-06-01 16:50:34 -0400

    third commit

--- calculateHireCost.js
+++ calculateHireCost.js
@@ -1,58 +1,13 @@
 // Calculate the cost of hiring personal for the dates specified
-var $date1 = $('#sdate');
-$('#sdate').text($date1);
-var date1 = $date1[0];
-
-var $date2 = $('#cdate');
-var date2 = $date2[0];
-$('#cdate').text($date2);
-
-var $rate = $('#rate');
-$('#rate').val($rate);
-var $hours = $('#hours');
-$('#hours').val($hours);
-
-var $theList = $('#theList');   // jQuery collection
-var theList = $theList[0];      // DOM node
-
-console.log($date1[0]);
-console.log(date1);
-console.log($date2[0]);
-console.log(date2);
-console.log($hours);
-console.log($rate);
 
 
-// convert date string to date
-date1 = new Date(date1);
-date2 = new Date(date2);
-
-console.log(date1);
-console.log(date2);
-
-// retrieve number of weeks
-var weeks = weeksBetween(date1, date2);
-
-console.log(weeks);
-
-var comp = 0;
-
-// calculate total hours and compensation
-comp = $rate * $hours * weeks;
-thours = $hours * weeks;
-
-console.log(comp);
-console.log(thours);
-
-// return total hours and compensation to web page
-$("#thours").val(thours);
-$("#compensation").val(comp);
-
+document.getElementById("submit").addEventListener("click", cost);
 
 
 // This function calculates the number of weeks between the two dates
 
-function weeksBetween(date1, date2) {
+function weeksBetween(date1, date2) 
+{
 
     // The number of milliseconds in one week
     var OneWeek = 1000 * 60 * 60 * 24 * 7;
@@ -66,12 +21,53 @@ function weeksBetween(date1, date2) {
 
     // Convert back to days and return
     return Math.round(difference_ms/OneWeek);
-
 }
 
-// parse a date in yyyy-mm-dd format
-// function parseDate(input) {
-//  var parts = input.match(/(\d+)/g);
-  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-//  return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
-// }
+function cost() 
+{	
+	var custname = document.getElementById("custname").value;
+	var address = document.getElementById("address").value;
+	var city = document.getElementById("city").value;
+	var state = document.getElementById("state").value;
+	var zip = document.getElementById("zip").value;
+	var email = document.getElementById("email").value;
+	var phone = document.getElementById("phone").value;
+	var position = document.getElementById("position").value;
+	var message = document.getElementById("message").value;
+
+	var sDate = document.getElementById("sDate").value;
+	var cDate = document.getElementById("cDate").value;
+
+	var rate = document.getElementById("rate").value;
+	var hours = document.getElementById("hours").value;
+	
+	console.log(date1);
+	console.log(date2);
+	console.log(hours);
+	console.log(rate);
+
+
+	// convert date string to date
+	var date1 = new Date(sDate);
+	var date2 = new Date(cDate);
+
+	console.log(date1);
+	console.log(date2);
+
+	// retrieve number of weeks
+	var weeks = weeksBetween(date1, date2);
+
+	console.log(weeks);
+
+	// calculate total hours and compensation
+	var comp = rate * hours * weeks;
+	var tHours = hours * weeks;
+
+	console.log(comp);
+	console.log(tHours);
+
+	// return total hours and compensation to web page
+	
+	document.getElementById("tHours").value = tHours;
+	document.getElementById("compensation").value = comp;
+}
